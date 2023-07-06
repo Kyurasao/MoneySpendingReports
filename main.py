b65f3fc4bb8de11e1daa4a87a784ca0ed0260a0e
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-06 13:59:33 +0400

    added manual adding of expenses

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ a report on the amount of expenses for the day will be issued.
 
 
 def main():
-    ...
+    expense_list = []
+    value = ' '
+    while value:
+        value = input('Израсходовынную сумму: ')
+        if value == '':
+            break
+        else:
+            expense_list.append(value)
+    print(expense_list)
 
 
 if __name__ == '__main__':
