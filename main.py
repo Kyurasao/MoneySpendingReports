cbd63331da7d60548d3ee595693198c5db8c90df
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-06 12:43:36 +0400

    fix #2 | retext discription

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
 This is a program for collecting expenses and issuing reports.
+Users will be asked for information about their daily expenses and
+a report on the amount of expenses for the day will be issued.
 """
 
 
