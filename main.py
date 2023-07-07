460309abec70797176e4286eea43cfbdb3af4c78
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-07 13:32:09 +0400

    added something

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ def main():
         if value == 'no':
             break
         else:
-            data_time = input('Enter the data and the time if the expense: ')
+            data_time = input('Enter the data and the time if the expense, dd.mm.yyyy hh:mm:ss: ')
             payer = input('Enter the payer: ')
             details['payer'] = payer
-            amount = input('Enter the amount: ')
+            amount = input('Enter the amount, example: ')
             details['amount'] = amount
-            category = input('Enter the category of the expense: ')
+            category = input('Enter the category of the expense, category list! : ')
             details['category'] = category
-            method = input('Enter the payment method: ')
+            method = input('Enter the payment method, card or cash: ')
             details['method'] = method
-            value = input('To enter new expense - text "yes", to exit - text "no"')
+            value = input('To enter new expense - text "yes", to exit - text "no": ')
             expense_dict[data_time] = details
-    print(expense_dict)
+
+            with open('db.json', 'a+') as f:
+                f.write(str(expense_dict) + '\n')
 
 
 if __name__ == '__main__':
