bea16f453d11e0759ae075a7f6fd6f90dc6752a8
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-12 10:34:55 +0400

    #3 | remade

--- main.py
+++ main.py
@@ -3,66 +3,74 @@ This is a program for collecting expenses and issuing reports.
 Users will be asked for information about their daily expenses and
 a report on the amount of expenses for the day will be issued.
 """
+import json
+import time
+from pathlib import Path
+from typing import List
+
+CATEGORIES = {
+    '01': 'health',
+    '02': 'clothes',
+    '03': 'eating out',
+    '04': 'food',
+    '05': 'trips',
+    '06': 'entertainments',
+    '07': 'connection',
+    '08': 'car',
+    '09': 'payments',
+    '99': 'other',
+}
+
+
+def create_expense(expenses: List):
+    details = {}
+
+    print(f"Let's enter expenses data...")
+    date_time_string = input("Enter time of expense | 'now' orr date in format [dd.mm.yyyy hh:mm:ss]: ")
+    date_time = time.strftime('%d.%m.%Y %H:%M:%S',
+                              time.localtime(time.time())) if date_time_string == 'now' else date_time_string
+    details['payer'] = input('Who payed: ')
+    details['amount'] = input('How much: ')
+    category_string = input('Enter the category of the expense (use numbers): \n'
+                            '[01] health | [02] clothes | [03] eating out | [04] food | '
+                            '[05] trips \n[06] entertainments | [07] connection | [08] car | '
+                            '[09] payments | [99] other :')
+
+    details['category'] = CATEGORIES.get(category_string, 'other')
+    details['method'] = input('Enter the payment method, card or cash:')
+
+    # adding details to list of expenses
+    expenses.append({date_time: details})
 
 
 def main():
+    # read db file
+    p = Path('db.json')
+    with p.open('r') as file:
+        file_data = file.read()
+        expenses: List = json.loads(file_data) if file_data else []
 
-    # def add_func():
-    #
-    # expense_dict = {}
-    # details = {}
-    # value = 'yes'
-    # while value == 'yes':
-    #     if value == 'no':
-    #         break
-    #     else:
-    #         data_time = input('Enter the data and the time if the expense, \ndd.mm.yyyy hh:mm:ss: \n')
-    #         payer = input('Enter the payer: \n')
-    #         details['payer'] = payer
-    #         amount = input('Enter the amount: \n')
-    #         details['amount'] = amount
-    #         category = input('Enter the category of the expense, '
-    #                          '\nhealth, clothes, eating out, food, trips, entertainments, connection, car, '
-    #                          'payments, other : \n')
-    #         details['category'] = category
-    #         method = input('Enter the payment method, \ncard or cash: \n')
-    #         details['method'] = method
-    #         value = input('To enter new expense - text "yes", to exit - text "no": \n')
-    #         expense_dict[data_time] = details
-    #         with open('db.json', 'a+') as f:
-    #             f.write(str(expense_dict) + '\n')
+    while True:
+        value = input("type: 'exit' to exit | 'list' to list all expenses "
+                      "| 'create' to create more | [number of the line] to edit ")
+        if value == 'exit':
+            break
+        elif value == 'create':
+            create_expense(expenses=expenses)
+        elif value == 'list':
+            for index, expense in enumerate(expenses):
+                print(f'[{index + 1}] {expense}')
+        elif value.isdigit():
+            if 0 < int(value) <= len(expenses):
+                print(expenses[int(value)-1])
+            else:
+                print(f'wrong value, we have only [1:{len(expenses)}]')
+        else:
+            print(f'Unknown value. Once again...')
 
-    # def edit_func():
-
-    try:
-        with open('db.json', 'r') as f:
-            # print('Choose one string to edit it')
-            i = 0
-            for line in f:
-                i += 1
-                print(i, line.strip())
-    except:
-        print('A database file not exist yet, add a expense to creat a new one')
-
-    num = int(input('Choose the number of string to edit it: \n'))
-    with open('db.json', 'r') as f:
-        for i, line in enumerate(f):
-            if i == num - 1:
-                print(line, end='')
-                break
-        if i != num - 1:
-            print('Строки с таким номером нет')
-
-    with open('db.json', 'r') as NewPrj_kv:
-        lines = NewPrj_kv.readlines()
-
-    # print(lines)
-    new_line = input('enter new line: ')
-    del lines[num]
-    lines.insert(2, new_line + '\n')
-
-    with open('db.json', 'w') as NewPrj_kv:
-        NewPrj_kv.writelines(lines)
+            # saving final data
+    with p.open('w+') as file:
+        json.dump(expenses, file)
 
 
 if __name__ == '__main__':
