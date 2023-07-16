a47df77991fd8a9ce27704b015cb2a82bd0e5c2d
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-16 18:53:04 +0400

    First stage for webserver is done.
    added uvicorn.run() for debugging
    
    added on_exit event handler
    
    added pydantic class Item
    
    added POST method create

--- webserver.py
+++ webserver.py
@@ -1,8 +1,32 @@
+import atexit
 import json
+import sys
+from datetime import datetime
 from pathlib import Path
-from typing import List
+from typing import List, Union, Optional
 
+import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+@atexit.register
+def on_exit(exception: Optional[str] = None, exit_code: Optional[int] = None):
+    # Perform cleanup or finalization tasks here
+    print("Exiting...")
+    p = Path('db.json')
+    with p.open('w+') as f:
+        json.dump(expenses, f)  # Raise SystemExit to trigger the exit event
+    raise SystemExit(exit_code)
+
+
+class Item(BaseModel):
+    time: Union[str, datetime]
+    payer: str
+    amount: str
+    category: str
+    method: str
+
 
 app = FastAPI()
 p = Path('db.json')
@@ -19,11 +43,28 @@ async def root():
 @app.get("/list")
 async def list_of_expenses():
     result = []
-    for index, expense in enumerate(expenses):
-        result.append({f'{index + 1}': expense})
+    for index, expense_ in enumerate(expenses):
+        result.append({f'{index + 1}': expense_})
     return {"expenses": result}
 
 
 @app.get("/expense/{item_id}")
 async def expense(item_id: int):
-    return {"item_id": expenses[int(item_id) - 1]}
+    return {"item_id": expenses[int(item_id) - 1]} if item_id in range(1, len(expenses) + 1) else {
+        "error": f"id [{item_id}] out of range [1:{len(expenses)}]"}
+
+
+@app.post("/create")
+async def create_item(item: Item):
+    # преобраховать данные из item в формат данных из db.json
+    # добавить преобразованные данные в expenses
+    details = {'payer': item.payer, 'amount': item.amount, 'category': item.category,
+               'method': item.method}
+    time_value = datetime.now() if item.time == 'now' else item.time
+    expenses.append({time_value: details})
+    return item
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    sys.exit(0)
